1d6ec8ea96e74ba11f05f84852bbdae7fa222c86
DornieDortsch <dornie.dortsch@gmail.com>
2018-12-14 10:57:53 +0100

    Initializes project

---
+++ README.md
@@ -0,0 +1,3 @@
+Sample Project to report Proxy Bug.
+
+Connecting to slack with node-slack-sdk behind a company proxy is not possible with Version > 4.4.
---
+++ index.js
@@ -0,0 +1,32 @@
+const HttpsProxyAgent = require('https-proxy-agent');
+const { RTMClient } = require('@slack/client');
+
+require('dotenv').config()
+
+const options = {
+    agent: new HttpsProxyAgent(process.env.PROXY_URL),
+    logLevel: 'debug',
+    logger: (level, message) => {
+        console.log(message);
+    }
+}
+
+const rtm = new RTMClient(process.env.TOKEN, options);
+
+rtm.start();
+
+rtm.on('error', error => {
+    console.error('Error!', error);
+});
+
+rtm.on('unable_to_rtm_start', error => {
+    console.error('unable_to_rtm_start!', error);
+});
+
+rtm.on('connecting', () => {
+    console.info('Connectin!');
+});
+
+rtm.on('connected', () => {
+    console.info('Connected!');
+});
